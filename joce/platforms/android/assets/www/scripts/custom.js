88655bb83b0575f948f2dd75e2dbdef564a51a81
MatiPruvost <matiaspruvost@gmail.com>
2015-05-20 21:34:18 -0300

    UP

--- joce/platforms/android/assets/www/scripts/custom.js
+++ joce/platforms/android/assets/www/scripts/custom.js
@@ -98,9 +98,12 @@ angular
             $location.path(path);
           };
           $scope.submit = function () {
+            var date = new Date(new Date().toUTCString());
+            date = date.toISOString();
             var jocex = {
               text:$scope.jocex,
-              joceId:$stateParams.joceId
+              joceId:$stateParams.joceId,
+              timeStamp: date
             };
             var number = null;
             var dbSize = 5 * 1024 * 1024; // 5Mb
@@ -116,15 +119,15 @@ angular
                   joceDb.push(results.rows.item(i))
                 }
                 $timeout(function(){
-                  number = joceDb[0].number
+                  number = joceDb[0].number;
                 });
               }, null);
             });
             db.transaction(function (tx) {
-              tx.executeSql("CREATE TABLE IF NOT EXISTS jocex(id INTEGER PRIMARY KEY ASC, text TEXT, joceId TEXT)", []);
+              tx.executeSql("CREATE TABLE IF NOT EXISTS jocex(id INTEGER PRIMARY KEY ASC, text TEXT, joceId TEXT, timeStamp TIMESTAMP)", []);
             });
             db.transaction(function (tx) {
-              tx.executeSql("INSERT INTO jocex (text, joceId) VALUES (?,?)",[jocex.text, jocex.joceId], function (tx, results) {
+              tx.executeSql("INSERT INTO jocex (text, joceId, timeStamp) VALUES (?,?,?)",[jocex.text, jocex.joceId, jocex.timeStamp], function (tx, results) {
               });
             });
             db.transaction(function (tx) {
@@ -139,7 +142,6 @@ angular
                     $location.path('/showJoce/'+$stateParams.joceId);
                   }
                   else{
-                    console.log(jocexsDb.length, number)
                     $location.path('/home');
                   }
                 });
@@ -147,10 +149,27 @@ angular
             });
           };
           function getJocexs (){
+            var timeNow = new Date(new Date().toUTCString());
+            var time = null;
             var dbSize = 5 * 1024 * 1024; // 5Mb
             var db = window.openDatabase("joceTest", "1.0", "Joce Test DB", dbSize);
             db.transaction(function (tx) {
-              tx.executeSql("CREATE TABLE IF NOT EXISTS jocex(id INTEGER PRIMARY KEY ASC, text TEXT, joceId TEXT)", []);
+              tx.executeSql("CREATE TABLE IF NOT EXISTS joce(id INTEGER PRIMARY KEY ASC, name TEXT, number INT, time INT, minumum INT, maximum INT)", []);
+            });
+            db.transaction(function (tx) {
+              tx.executeSql('SELECT * FROM joce WHERE id=?', [$stateParams.joceId], function (tx, results) {
+                var joceDb = [];
+                var len = results.rows.length;
+                for (var i = 0; i < len; i++) {
+                  joceDb.push(results.rows.item(i))
+                }
+                $timeout(function(){
+                  time = joceDb[0].time;
+                });
+              }, null);
+            });
+            db.transaction(function (tx) {
+              tx.executeSql("CREATE TABLE IF NOT EXISTS jocex(id INTEGER PRIMARY KEY ASC, text TEXT, joceId TEXT, timeStamp TIMESTAMP)", []);
             });
             db.transaction(function (tx) {
               tx.executeSql('SELECT * FROM jocex WHERE joceId=?', [$stateParams.joceId], function (tx, results) {
@@ -159,12 +178,29 @@ angular
                 for (var i = 0; i < len; i++) {
                   jocexsDb.push(results.rows.item(i))
                 }
+                var lastestJocex = jocexsDb[jocexsDb.length -1]
                 $timeout(function(){
-                  if(jocexsDb[jocexsDb.length -1] === undefined){
+                  if(lastestJocex === undefined){
                     var jdb = {text:"There isn't any jocex for this joce"};
                     $scope.lastestJocex = jdb;
+                    $scope.disabled = false;
                   }
                   else{
+                    var validTime = new Date(lastestJocex.timeStamp);
+                    validTime.setDate(validTime.getDate() + time);
+                    if(validTime > timeNow){
+                      var validedTime = new Date(validTime);
+                      var date = validedTime.toLocaleDateString();
+                      var time_ = validedTime.toLocaleTimeString();
+                      var text1 = "You need wait until the ";
+                      var text2 = " at ";
+                      var text = text1.concat(date, text2, time_);
+                      $scope.disabled = true;
+                      alert(text);
+                    }
+                    else{
+                      $scope.disabled = false;
+                    }
                     $scope.lastestJocex = jocexsDb[jocexsDb.length -1];
                   }
                 });
@@ -285,6 +321,9 @@ angular
             });
             db.transaction(function (tx) {
               tx.executeSql('SELECT * FROM joce', [], function (tx, results) {
+                var date = new Date(new Date().toUTCString());
+                date = date.toISOString();
+                console.log(date)
                 var jocesDb = [];
                 var len = results.rows.length;
                 for (var i = 0; i < len; i++) {
@@ -313,7 +352,7 @@ angular
             var dbSize = 5 * 1024 * 1024; // 5Mb
             var db = window.openDatabase("joceTest", "1.0", "Joce Test DB", dbSize);
             db.transaction(function (tx) {
-              tx.executeSql("CREATE TABLE IF NOT EXISTS joce(id INTEGER PRIMARY KEY ASC, name TEXT, number INT, time INT, minumum INT, maximum INT)", []);
+              tx.executeSql("CREATE TABLE IF NOT EXISTS jocex(id INTEGER PRIMARY KEY ASC, text TEXT, joceId TEXT, timeStamp TIMESTAMP)", []);
             });
             db.transaction(function (tx) {
               tx.executeSql('SELECT * FROM jocex WHERE joceId=?', [$stateParams.joceId], function (tx, results) {
