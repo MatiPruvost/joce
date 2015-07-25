5b1dd2da2371109e7dc8e91794c35620b2b1eede
MatiPruvost <matiaspruvost@gmail.com>
2015-07-25 13:26:11 -0300

    Change names

--- mjoce/app/modules/core/controllers/about.js
+++ mjoce/app/modules/core/controllers/about.js
@@ -10,8 +10,5 @@ angular
     .module('core')
     .controller('aboutController', ['$scope', '$location', '$stateParams', '$timeout', '$cordovaSocialSharing',
         function($scope, $location, $stateParams, $timeout, $cordovaSocialSharing) {
-          $scope.shareAnywhere = function() {
-            $cordovaSocialSharing.share('You can download Joce here', null, null, 'http://www.google.com');
-          }
         }
     ]);
